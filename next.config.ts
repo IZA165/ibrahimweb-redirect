a35d336f8f710c3600e1f5c1539ea61a3f7e3b8f
Ibrahim <144132079+IZA165@users.noreply.github.com>
2025-10-02 18:48:47 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      // Enforce www
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "ibrahimweb.co.uk",
+          },
+        ],
+        destination: "https://www.ibrahimweb.co.uk/:path*",
+        permanent: true,
+      },
+      // Catch all other routes (already pointing to this project)
+      {
+        source: "/:path*",
+        destination: "https://www.ibrahimweb.co.uk/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
